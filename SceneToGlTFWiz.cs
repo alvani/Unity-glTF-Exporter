870dc7997b8b5c25300cab391850846a0c7a3147
Alvani Wiwoho <alpha2_yui@yahoo.com>
2017-02-20 14:35:44 +0700

    Set root position to zero

--- SceneToGlTFWiz.cs
+++ SceneToGlTFWiz.cs
@@ -748,10 +748,10 @@ public class SceneToGlTFWiz : EditorWindow
 
 				// do not use global position if rtc is defined
 				Vector3 pos = Vector3.zero;
-				if (writer.RTCCenter == null) 
-				{
-					pos = tr.localPosition;
-				}
+//				if (writer.RTCCenter == null) 
+//				{
+//					pos = tr.localPosition;
+//				}
 
 				mat = mat * Matrix4x4.TRS(pos, tr.localRotation, tr.localScale);
 				node.matrix = new GlTF_Matrix(mat);
