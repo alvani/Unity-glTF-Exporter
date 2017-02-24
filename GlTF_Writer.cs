14d93df1b0e1495ba0b384d70c609bca478a9e1a
Alvani Wiwoho <alpha2_yui@yahoo.com>
2017-02-24 15:28:03 +0700

    Implement mat4 buffer view

--- GlTF_Writer.cs
+++ GlTF_Writer.cs
@@ -18,6 +18,7 @@ public class GlTF_Writer {
 	public static GlTF_BufferView vec2BufferView = new GlTF_BufferView("vec2BufferView");
 	public static GlTF_BufferView vec3BufferView = new GlTF_BufferView("vec3BufferView");
 	public static GlTF_BufferView vec4BufferView = new GlTF_BufferView("vec4BufferView");
+	public static GlTF_BufferView mat4BufferView = new GlTF_BufferView("mat4BufferView");
 	public static List<GlTF_BufferView> bufferViews = new List<GlTF_BufferView>();	
 	public static List<GlTF_Camera> cameras = new List<GlTF_Camera>();
 	public static List<GlTF_Light> lights = new List<GlTF_Light>();
@@ -147,12 +148,14 @@ public class GlTF_Writer {
 		bufferViews.Add (vec2BufferView);
 		bufferViews.Add (vec3BufferView);
 		bufferViews.Add (vec4BufferView);
+		bufferViews.Add (mat4BufferView);
 
 		ushortBufferView.bin = binary;
 		floatBufferView.bin = binary;
 		vec2BufferView.bin = binary;
 		vec3BufferView.bin = binary;
 		vec4BufferView.bin = binary;
+		mat4BufferView.bin = binary;
 
 		// write memory streams to binary file
 		ushortBufferView.byteOffset = 0;
@@ -160,6 +163,7 @@ public class GlTF_Writer {
 		vec2BufferView.byteOffset = floatBufferView.byteOffset + floatBufferView.byteLength;
 		vec3BufferView.byteOffset = vec2BufferView.byteOffset + vec2BufferView.byteLength;
 		vec4BufferView.byteOffset = vec3BufferView.byteOffset + vec3BufferView.byteLength;
+		mat4BufferView.byteOffset = vec4BufferView.byteOffset + vec4BufferView.byteLength;
 
 		jsonWriter.Write ("{\n");
 		IndentIn();
@@ -666,6 +670,7 @@ public class GlTF_Writer {
 		vec2BufferView.memoryStream.WriteTo (binFile);
 		vec3BufferView.memoryStream.WriteTo (binFile);
 		vec4BufferView.memoryStream.WriteTo (binFile);
+		mat4BufferView.memoryStream.WriteTo(binFile);
 
 		binFile.Flush();
 		if (binary)
